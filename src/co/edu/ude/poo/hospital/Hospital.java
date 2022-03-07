5f71c6ee5302ef492d2ab661a77ed13deadccd5c
Nicolas Julian Torres <71136123+JulianTorres1@users.noreply.github.com>
2022-03-06 22:21:19 -0500

    comenzando con la intertfas freafica
    solo comence con la interfas me falta todo

--- src/co/edu/ude/poo/hospital/Hospital.java
+++ src/co/edu/ude/poo/hospital/Hospital.java
@@ -11,6 +11,7 @@ import java.util.Scanner;
 
 
 
+import co.edu.ude.poo.hospital.vistas.gui.VentanaPrincipal;
 
 
 
@@ -23,69 +24,10 @@ import java.util.Scanner;
 public class Hospital {
     public static void main(String[] args) {
         
-        Scanner sc = new Scanner(System.in);
-        Boolean Estado = true;
-        int opt = 0;
         
-        while(Estado){
-            
-            System.out.println("""
-                               Que desea hacer ahora:
-                               1. Ingresar Un Tipo De Usuario
-                               2. Mostrar un grupo de usuarios
-                               3. Editar un Usuario
-                               4. Eliminar Un Usuario
-                               5. Salir Del Programa
-                               """);
-            
-            opt = sc.nextInt();
-            
-            if(opt == 1){
-                // Metodo Que se encarga de recolectar la informacion
-                infoColector.recolect();
-
-            
-            
-            }
-            if(opt == 2){
-                // Metodo que se Encarga del funcionamiento del CRUD
-                GestionDeAlmacenamiento.ListarBD();
-            }
-            
-            if (opt == 3) {
-                
-                System.out.println("Ingrese la ID del Usuario a Editar: \n"
-                        + "  ");
-                
-                int idE = sc.nextInt();
-                
-                GestionDeAlmacenamiento.EditarUsuario(idE);
-                
-                
-            }
-            if (opt == 4) {
-                
-                System.out.println("Ingrese la ID del usuario a eliminar:");
-                int idD = sc.nextInt();
-                
-                GestionDeAlmacenamiento.Destruir(idD);
-                
-                
-            }
-            
-            if (opt == 5) {
-                
-                System.out.println("Gracias por su visita! :D");
-                
-                Estado = false;
-                
-            }
-            
-            
-            
-            
+        VentanaPrincipal.main(args);
         
-        }
+        
         
         
         
---
+++ src/co/edu/ude/poo/hospital/vistas/gui/VentanaCrudUsuarios.java
@@ -0,0 +1,73 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package co.edu.ude.poo.hospital.vistas.gui;
+
+import co.edu.ude.poo.hospital.modelo.entidades.Users_1;
+import co.edu.ude.poo.hospital.servicios.Users_1JpaController;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author julia
+ */
+public class VentanaCrudUsuarios {
+    
+    //objetos de la clase Usuario_1
+    private Users_1 u = new Users_1();
+    private JFrame jFrame = new JFrame();
+    
+    public String InsertarUsuario(
+            String nombre,
+            String direccion,
+            String telefono,
+            String poblacion,
+            String provincia,
+            String codigoPostal,
+            String nif,
+            String numeroSeguridaSocial
+    ){
+        
+        
+        String mensaje = "";
+        
+        EntityManagerFactory conexionBD = Persistence.createEntityManagerFactory("HospitalPU");
+        Users_1JpaController crudUser = new Users_1JpaController(conexionBD);
+        
+        
+        //asignamos los datos al objeto
+        
+        try {
+            u.setNombre(nombre);
+            u.setDireccion(direccion);
+            u.setTelefono(telefono);
+            u.setPoblacion(poblacion);
+            u.setProvincia(provincia);
+            u.setCodigoPostal(codigoPostal);
+            u.setNif(nif);
+            u.setNumeroSeguridadSocial(numeroSeguridaSocial);
+        
+            // asignamo el objeto a el metodo create
+            crudUser.create(u);
+            
+            JOptionPane.showMessageDialog(jFrame, "Datos Guardados Correctamente!");
+        } catch (Exception e) {
+            System.out.println("Error: "+ e);
+            JOptionPane.showMessageDialog(jFrame, "Error al guardar los datos");
+            
+            
+        }
+        
+        
+        
+    
+        return mensaje;
+    }
+    
+    
+    
+}
